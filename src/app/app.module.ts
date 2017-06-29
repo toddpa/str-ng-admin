03d5e0153c07030957e7d08c917e5dd1b1863b62
Paul Todd <pault@fleetway.com>
2017-06-29 14:14:49 +0100

    Integrate the KendoUI Angular grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AppState, InternalStateType } from './app.service';
 import { GlobalState } from './global.state';
 import { NgaModule } from './theme/nga.module';
 import { PagesModule } from './pages/pages.module';
+import { GridModule } from '@progress/kendo-angular-grid';
 
 
 // Application wide providers
@@ -47,6 +48,7 @@ export type StoreType = {
     NgaModule.forRoot(),
     NgbModule.forRoot(),
     PagesModule,
+    GridModule,
     routing
   ],
   providers: [ // expose our Services and Providers into Angular's dependency injection
