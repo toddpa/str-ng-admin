313777da8c568c94246e65de0e0d9ebaa333abb7
Paul Todd <pault@fleetway.com>
2017-06-29 13:04:36 +0100

    Added modules

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,15 +1,43 @@
-import { NgModule }      from '@angular/core';
-import { CommonModule }  from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { routing }       from './pages.routing';
+import { routing } from './pages.routing';
 import { NgaModule } from '../theme/nga.module';
 import { AppTranslationModule } from '../app.translation.module';
 
 import { Pages } from './pages.component';
+import { SRTDataService } from '../service/SRTDataService';
+import { Configuration } from '../service/Configuration';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { InvestorsComponent } from './investors/investors.component';
+import { ProductComponent } from './product/product.component';
+import { ReconciliationsComponent } from './product/reconciliations/reconciliations.component';
+import { PortfoliosComponent } from './product/portfolios/portfolios.component';
+import { ClensingComponent } from './product/clensing/clensing.component';
+import { FactorsComponent } from './product/factors/factors.component';
+
 
 @NgModule({
-  imports: [CommonModule, AppTranslationModule, NgaModule, routing],
-  declarations: [Pages]
+  imports: [
+    GridModule,
+    CommonModule,
+    AppTranslationModule,
+    NgaModule,
+    routing
+  ],
+  declarations: [
+    Pages,
+    InvestorsComponent,
+    ProductComponent,
+    ReconciliationsComponent,
+    PortfoliosComponent,
+    ClensingComponent,
+    FactorsComponent
+  ],
+  providers: [
+    SRTDataService,
+    Configuration
+  ]
 })
 export class PagesModule {
 }
